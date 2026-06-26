4f0bdcdcd3b5bffdd7ba971e73e7c31a178baee5
k-gloire <g.umuhendak@alustudent.com>
2026-06-26 17:03:11 +0200

    Fix concat edges task

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-str = str[39:66] + str[106:113] + str[:6]
-print(str)
+#!/usr/bin/env python3
+
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+
+print(str[39:66] + " " + str[107:112] + str[:6])
