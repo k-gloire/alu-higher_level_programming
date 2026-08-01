c0395978ebe9f55a85b4f7d15550e99c3d48e985
k-gloire <g.umuhendak@alustudent.com>
2026-08-01 19:15:55 +0200

    Add -L to follow redirects in 1-body.sh

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request and display only the body if status is 200
-[ "$(curl -s -o /tmp/curl_body -w '%{http_code}' "$1")" = "200" ] && cat /tmp/curl_body
+# Send a GET request, follow redirects, and display the body if status is 200
+[ "$(curl -sL -o /tmp/curl_body -w '%{http_code}' "$1")" = "200" ] && cat /tmp/curl_body
