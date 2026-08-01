3a2f935758981b59c82cd661868b650fcf6a2017
k-gloire <g.umuhendak@alustudent.com>
2026-08-01 18:44:10 +0200

    Fix pycodestyle line length in query

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -11,9 +11,8 @@ if __name__ == "__main__":
         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
     )
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC".format(
-        sys.argv[4]
-    )
+    query = ("SELECT * FROM states WHERE BINARY name = '{}' "
+              "ORDER BY id ASC".format(sys.argv[4]))
     cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
